2ad976c81099f16d488a54009f0c9cec2a726b34
Wictor Sundström <wictor.sundstrom@plejd.com>
2023-12-02 07:33:51 +0100

    Old local data

--- 2022/main.py
+++ 2022/main.py
@@ -1,5 +1,6 @@
 from Solution.day01 import *
 from Solution.day02 import *
+from Solution.day03 import *
 
 day02 = 'Input/day02.txt'
 
@@ -8,3 +9,6 @@ day1_solution()
 
 # Day 02
 day2_solution()
+
+# Day 03
+day3_solution()
